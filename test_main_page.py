69b770ca576b26aa60305f86d2084f0bebbc5371
Egor1188 <egor1188@gmail.com>
2022-12-27 04:31:42 +0200

    Added new locators and tests for main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,15 @@
-from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/uk/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "https://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
     page.open()
-    # page.go_to_login_page()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser=browser, url=browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
     link = "https://selenium1py.pythonanywhere.com/"
