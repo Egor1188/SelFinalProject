c1ab3c6b756386ce4cfc9c88859b637c5a414b81
Egor1188 <egor1188@gmail.com>
2022-12-27 04:31:42 +0200

    Added new locators and tests for main page

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url
+
+    def should_be_login_form(self):
+
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM)
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM)
